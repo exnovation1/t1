281fe6e0e692ed0c2738b834ce001b26d8bc9b5c
exnovation1 <biswajit.deb@exnovation.in>
2020-08-25 17:07:53 +0530

    First Commite

---
+++ app/src/main/java/com/wishster/mygitcheck/MainActivity.java
@@ -0,0 +1,17 @@
+package com.wishster.mygitcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wishster.mymodel.Abc;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ mymodel/src/main/java/com/wishster/mymodel/Abc.java
@@ -0,0 +1,5 @@
+package com.wishster.mymodel;
+
+public class Abc {
+    public static  String a="";
+}
